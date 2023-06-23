f10d722eafd3d80f1cdeb8a1ae57e2e5674ff3d1
Alex Hedley <alex.hedley@opencastsoftware.com>
2023-06-23 11:09:35 +0100

    Added custom page

---
+++ src/Constants.cs
@@ -0,0 +1,10 @@
+namespace MySite;
+
+public class Constants
+{
+    public const string SiteTitle = "Your Website";
+    public const string SiteUri = "https://example.com";
+    public const string BlogTitle = "Your Blog";
+    public const string BlogPath = "blog"; // ex. "blog" indicates https://example.com/blog/
+    public const int PostsPerPage = 10;
+}
